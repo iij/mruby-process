f167ab0839001c20ea87224ab23fa08aa52cd88a
Tomoyuki Sahara <tsahara@iij.ad.jp>
2013-09-30 11:24:51 +0900

    sort by method name.

--- test/process.rb
+++ test/process.rb
@@ -2,17 +2,17 @@ assert('Process') do
   Process.class == Module
 end
 
+assert('Process.kill') do
+  # to be sure we won't kill an innocent process!
+  Process.pid > 0 and Process.kill(0, Process.pid)
+end
+
 assert('Process.pid') do
   Process.pid is_a? Integer and Process.pid > 0
 end
 
 assert('Process.ppid') do
   Process.ppid is_a? Integer and Process.ppid > 0
-end
-
-assert('Process.kill') do
-  # to be sure we won't kill an innocent process!
-  Process.pid > 0 and Process.kill(0, Process.pid)
 end
 
 assert('$$') do

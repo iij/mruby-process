7308e8f4737cb33e3a3d92e60169983a6ebddd08
Tomoyuki Sahara <tsahara@iij.ad.jp>
2016-12-01 14:54:44 +0900

    merge wnohang.rb to process.rb

--- test/process.rb
+++ test/process.rb
@@ -18,3 +18,21 @@ end
 assert('$$') do
   $$ == Process.pid
 end
+
+assert("Process.fork with WNOHANG") do
+  pid = fork {
+    loop {}
+  }
+  p, s = Process.waitpid2(pid, Process::WNOHANG)
+  assert_nil(p)
+  assert_nil(s)
+
+  Process.kill :TERM, pid
+  loop {
+    # wait until the process completely killed with non-block mode
+    p, s = Process.waitpid2(pid, Process::WNOHANG)
+    break if p
+  }
+  assert_equal(pid, p)
+  assert_true(s.signaled?)
+end

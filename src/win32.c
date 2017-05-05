3a17211fe628aae18a4884fab78cbd1285026e20
Sebastián Katzer <katzer.sebastian@googlemail.com>
2017-05-05 13:31:24 +0200

    Improved waitpid for win32

--- src/win32.c
+++ src/win32.c
@@ -24,53 +24,60 @@
 #include "process.h"
 
 #include <windows.h>
+#include <process.h>
+
+#define MAXCHILDNUM 256 /* max num of child processes */
 
 /* License: Ruby's */
 static struct ChildRecord {
-  HANDLE hProcess;
-  pid_t pid;
-} ChildRecord[256];
+    HANDLE hProcess;
+    pid_t pid;
+} ChildRecord[MAXCHILDNUM];
 
 /* License: Ruby's */
 #define FOREACH_CHILD(v) do { \
-  struct ChildRecord* v; \
-  for (v = ChildRecord; v < ChildRecord + sizeof(ChildRecord) / sizeof(ChildRecord[0]); ++v)
+    struct ChildRecord* v; \
+    for (v = ChildRecord; v < ChildRecord + sizeof(ChildRecord) / sizeof(ChildRecord[0]); ++v)
 #define END_FOREACH_CHILD } while (0)
 
 static FARPROC get_proc_address(const char *module, const char *func, HANDLE *mh);
+static pid_t poll_child_status(struct ChildRecord *child, int *stat_loc);
 static struct ChildRecord *FindChildSlot(pid_t pid);
+static struct ChildRecord *FindChildSlotByHandle(HANDLE h);
+static void CloseChildHandle(struct ChildRecord *child);
 
 int
 fork(void)
 {
-  return -1;
+    return -1;
 }
 
 /* License: Ruby's */
 pid_t
 getppid(void)
 {
-  typedef long (WINAPI query_func)(HANDLE, int, void *, ULONG, ULONG *);
-  static query_func *pNtQueryInformationProcess = (query_func *)-1;
-  pid_t ppid = 0;
+    typedef long (WINAPI query_func)(HANDLE, int, void *, ULONG, ULONG *);
+    static query_func *pNtQueryInformationProcess = (query_func *) - 1;
+    pid_t ppid = 0;
 
-    if (pNtQueryInformationProcess == (query_func *)-1)
-      pNtQueryInformationProcess = (query_func *)get_proc_address("ntdll.dll", "NtQueryInformationProcess", NULL);
+    if (pNtQueryInformationProcess == (query_func *) - 1)
+        pNtQueryInformationProcess = (query_func *)get_proc_address("ntdll.dll", "NtQueryInformationProcess", NULL);
 
     if (pNtQueryInformationProcess) {
-      struct {
-        long ExitStatus;
-        void* PebBaseAddress;
-        uintptr_t AffinityMask;
-        uintptr_t BasePriority;
-        uintptr_t UniqueProcessId;
-        uintptr_t ParentProcessId;
-      } pbi;
-      ULONG len;
-      long ret = pNtQueryInformationProcess(GetCurrentProcess(), 0, &pbi, sizeof(pbi), &len);
-      if (!ret) {
-        ppid = pbi.ParentProcessId;
-      }
+        struct {
+            long ExitStatus;
+            void* PebBaseAddress;
+            uintptr_t AffinityMask;
+            uintptr_t BasePriority;
+            uintptr_t UniqueProcessId;
+            uintptr_t ParentProcessId;
+        } pbi;
+
+        ULONG len;
+        long ret = pNtQueryInformationProcess(GetCurrentProcess(), 0, &pbi, sizeof(pbi), &len);
+
+        if (!ret)
+            ppid = pbi.ParentProcessId;
     }
 
     return ppid;
@@ -80,117 +87,217 @@ getppid(void)
 pid_t
 waitpid(pid_t pid, int *stat_loc, int options)
 {
-  DWORD timeout;
-  HANDLE hProc;
-  struct ChildRecord* child;
+    DWORD timeout;
+    HANDLE hProc;
+    struct ChildRecord* child;
 
-  child = FindChildSlot(pid);
-  if (child) {
-    hProc = child->hProcess;
-  }
-  if (hProc == NULL || hProc == INVALID_HANDLE_VALUE) {
-    return -1;
-  }
+    /* Artistic or GPL part start */
+    if (options == WNOHANG)
+        timeout = 0;
+    else
+        timeout = INFINITE;
+    /* Artistic or GPL part end */
 
-  /* Artistic or GPL part start */
-  if (options == WNOHANG) {
-    timeout = 0;
-  }
-  else {
-    timeout = INFINITE;
-  }
-  /* Artistic or GPL part end */
+    if (pid == -1) {
+        int count = 0;
+        int ret;
+        HANDLE targets[MAXCHILDNUM];
+        struct ChildRecord* cause;
 
-  return (WaitForSingleObject(hProc, timeout) == WAIT_OBJECT_0) ? pid : -1;
+        FOREACH_CHILD(child) {
+            if (!child->pid || child->pid < 0) continue;
+            if ((pid = poll_child_status(child, stat_loc))) return pid;
+            targets[count++] = child->hProcess;
+        } END_FOREACH_CHILD;
+
+        if (!count)
+            return -1;
+
+        ret = WaitForMultipleObjects(count, targets, FALSE, timeout);
+        if (ret == WAIT_TIMEOUT) return 0;
+        if ((ret -= WAIT_OBJECT_0) == count) return -1;
+        if (ret > count) return -1;
+
+        cause = FindChildSlotByHandle(targets[ret]);
+        if (!cause) return -1;
+
+        return poll_child_status(cause, stat_loc);
+    }
+    else {
+        child = FindChildSlot(pid);
+        int retried = 0;
+
+        if (!child || child->hProcess == INVALID_HANDLE_VALUE)
+            return -1;
+
+        while (!(pid = poll_child_status(child, stat_loc))) {
+            /* wait... */
+            int ret = WaitForMultipleObjects(1, &child->hProcess, FALSE, timeout);
+
+            if (ret == WAIT_OBJECT_0 + 1) return -1; /* maybe EINTR */
+            if (ret != WAIT_OBJECT_0) {
+                /* still active */
+                if (options & WNOHANG) {
+                    pid = 0;
+                    break;
+                }
+                ++retried;
+            }
+        }
+
+        if (pid == -1 && retried) pid = 0;
+    }
+
+    return pid;
 }
 
 /* License: Ruby's */
 int
 kill(pid_t pid, int sig)
 {
-  pid_t ret = 0;
-  DWORD ctrlEvent;
-  HANDLE hProc;
-  struct ChildRecord* child;
+    pid_t ret = 0;
+    DWORD ctrlEvent;
+    HANDLE hProc;
+    struct ChildRecord* child;
 
-  if (pid < 0 || (pid == 0 && sig != SIGINT)) {
-    return -1;
-  }
+    if (pid < 0 || (pid == 0 && sig != SIGINT))
+        return -1;
 
-  if ((unsigned int)pid == GetCurrentProcessId() &&
-    (sig != 0 && sig != SIGKILL)) {
-    ret = raise(sig);
-    return ret;
-  }
-
-  switch (sig) {
-    case 0:
-      hProc = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, (DWORD)pid);
-      if (hProc == NULL || hProc == INVALID_HANDLE_VALUE) {
-        ret = -1;
-      }
-      else {
-        CloseHandle(hProc);
-      }
-      break;
-
-    case SIGINT:
-      ctrlEvent = CTRL_C_EVENT;
-      if (pid != 0) {
-        ctrlEvent = CTRL_BREAK_EVENT;
-      }
-      if (!GenerateConsoleCtrlEvent(ctrlEvent, (DWORD)pid)) {
-        ret = -1;
-      }
-      break;
-
-    case SIGKILL:
-      child = FindChildSlot(pid);
-      if (child) {
-        hProc = child->hProcess;
-      }
-      else {
-        hProc = OpenProcess(PROCESS_TERMINATE | PROCESS_QUERY_INFORMATION, FALSE, (DWORD)pid);
-      }
-      if (hProc == NULL || hProc == INVALID_HANDLE_VALUE) {
-        ret = -1;
-      }
-      else {
-        DWORD status;
-        if (!GetExitCodeProcess(hProc, &status)) {
-          ret = -1;
-        }
-        else if (status == STILL_ACTIVE) {
-          if (!TerminateProcess(hProc, 0)) {
-            ret = -1;
-          }
-        }
-        else {
-          ret = -1;
-        }
-        if (!child) {
-          CloseHandle(hProc);
-        }
-      break;
+    if ((unsigned int)pid == GetCurrentProcessId() && (sig != 0 && sig != SIGKILL)) {
+        ret = raise(sig);
+        return ret;
     }
 
+    switch (sig) {
+        case 0:
+            hProc = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, (DWORD)pid);
+
+            if (hProc == NULL || hProc == INVALID_HANDLE_VALUE) {
+                ret = -1;
+            }
+            else {
+                CloseHandle(hProc);
+            }
+
+            break;
+
+        case SIGINT:
+            ctrlEvent = CTRL_C_EVENT;
+
+            if (pid != 0) ctrlEvent = CTRL_BREAK_EVENT;
+            if (!GenerateConsoleCtrlEvent(ctrlEvent, (DWORD)pid)) ret = -1;
+
+            break;
+
+        case SIGKILL:
+            child = FindChildSlot(pid);
+
+            if (child) {
+                hProc = child->hProcess;
+            }
+            else {
+                hProc = OpenProcess(PROCESS_TERMINATE | PROCESS_QUERY_INFORMATION, FALSE, (DWORD)pid);
+            }
+
+            if (hProc == NULL || hProc == INVALID_HANDLE_VALUE) {
+                ret = -1;
+            }
+            else {
+                DWORD status;
+
+                if (!GetExitCodeProcess(hProc, &status)) {
+                    ret = -1;
+                }
+                else if (status == STILL_ACTIVE) {
+                    if (!TerminateProcess(hProc, 0)) {
+                        ret = -1;
+                    }
+                }
+                else {
+                    ret = -1;
+                }
+
+                if (!child) {
+                    CloseHandle(hProc);
+                }
+            }
+
+            break;
+
     default:
-      ret = -1;
+        ret = -1;
   }
 
   return ret;
 }
 
 /* License: Ruby's */
+static pid_t
+poll_child_status(struct ChildRecord *child, int *stat_loc)
+{
+    DWORD exitcode;
+    DWORD err;
+
+    if (!GetExitCodeProcess(child->hProcess, &exitcode)) {
+        /* If an error occurred, return immediately. */
+    error_exit:
+        CloseChildHandle(child);
+        return -1;
+    }
+
+    if (exitcode != STILL_ACTIVE) {
+        pid_t pid;
+
+        /* If already died, wait process's real termination. */
+        if (WaitForSingleObject(child->hProcess, INFINITE) != WAIT_OBJECT_0) {
+            goto error_exit;
+        }
+
+        pid = child->pid;
+        CloseChildHandle(child);
+
+        if (stat_loc)
+            *stat_loc = exitcode << 8;
+
+        return pid;
+    }
+
+    return 0;
+}
+
+/* License: Ruby's */
 static struct ChildRecord *
 FindChildSlot(pid_t pid)
 {
-  FOREACH_CHILD(child) {
-    if (child->pid == pid) {
-      return child;
-    }
-  } END_FOREACH_CHILD;
-  return NULL;
+    FOREACH_CHILD(child) {
+        if (pid == -1 || child->pid == pid)
+            return child;
+    } END_FOREACH_CHILD;
+
+    return NULL;
+}
+
+/* License: Ruby's */
+static struct ChildRecord *
+FindChildSlotByHandle(HANDLE h)
+{
+    FOREACH_CHILD(child) {
+        if (child->hProcess == h)
+            return child;
+    } END_FOREACH_CHILD;
+
+    return NULL;
+}
+
+/* License: Ruby's */
+static void
+CloseChildHandle(struct ChildRecord *child)
+{
+    HANDLE h        = child->hProcess;
+    child->hProcess = NULL;
+    child->pid      = 0;
+
+    CloseHandle(h);
 }
 
 /* License: Ruby's */
@@ -220,29 +327,29 @@ get_proc_address(const char *module, const char *func, HANDLE *mh)
 mrb_value
 mrb_argv0(mrb_state *mrb)
 {
-  TCHAR argv0[MAX_PATH + 1];
+    TCHAR argv0[MAX_PATH + 1];
 
-  GetModuleFileName(NULL, argv0, MAX_PATH + 1);
+    GetModuleFileName(NULL, argv0, MAX_PATH + 1);
 
-  return mrb_str_new_cstr(mrb, argv0);
+    return mrb_str_new_cstr(mrb, argv0);
 }
 
 mrb_value
 mrb_progname(mrb_state *mrb)
 {
-  TCHAR argv0[MAX_PATH + 1];
-  char *progname;
+    TCHAR argv0[MAX_PATH + 1];
+    char *progname;
 
-  GetModuleFileName(NULL, argv0, MAX_PATH + 1);
+    GetModuleFileName(NULL, argv0, MAX_PATH + 1);
 
-  progname = strrchr(argv0, '\\');
+    progname = strrchr(argv0, '\\');
 
-  if (progname)
-    progname++;
-  else
-    progname = argv0;
+    if (progname)
+        progname++;
+    else
+        progname = argv0;
 
-  return mrb_str_new_cstr(mrb, progname);
+    return mrb_str_new_cstr(mrb, progname);
 }
 
 int
